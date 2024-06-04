30d2015863bf0e03abfdc1e492ed69fbb90f1e29
Adrian Alcocer <jadrianaldo@outlook.com>
2024-06-03 22:08:50 -0600

    Se configuró la conexión y se valido la comunicación exitosa a la base de datos

--- aplicacion.py
+++ aplicacion.py
@@ -1,9 +1,27 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+import mysql.connector # type: ignore
 
 app = Flask(__name__)
+
+# Configuración de la base de datos
+db_config = {
+    'user': 'root',
+    'password': 'YTsFDJaMfO0zJakbd9PJx0UEfBfotoU3neTCwpuWuZeCvWChwWKbU6yK65zRaVCw',
+    'host': 'localhost',
+    'database': 'registro_empleados'
+}
+
 @app.route('/')
 def index():
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+    sql = '''INSERT INTO registroDatos (nombre, apellidoPaterno, apellidoMaterno, edad, lugarNacimiento, email, foto) 
+             VALUES ("Jesús Adrián", "Alcocer", "Dóñez", 23, "Monterrey", "jadrianalcocer11@gmail.com", "foto.png");'''
+    cursor.execute(sql)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
     return render_template('registroEmpleados/index.html')
 
 if __name__ == '__main__':
