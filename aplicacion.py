f076565ef13a4f5a95ec559a8e747b34d8061d65
Adrian Alcocer <jadrianaldo@outlook.com>
2024-06-04 23:35:42 -0600

    Creación de los archivos create.html y edit.html, desarrollo del archivo create.html con el método POST

--- aplicacion.py
+++ aplicacion.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mysql.connector # type: ignore
 
 app = Flask(__name__)
@@ -23,6 +23,37 @@ def index():
     connection.close()
 
     return render_template('registroEmpleados/index.html')
+
+
+@app.route('/create')
+def create():
+    return render_template('registroEmpleados/create.html')
+
+@app.route('/data', methods=['POST'])
+def storage():
+
+    _nombre=request.form['txtNombre']
+    _apellidoPaterno=request.form['txtApellidoPaterno']
+    _apellidoMaterno=request.form['txtApellidoMaterno']
+    _edad=request.form['txtEdad']
+    _lugarNacimiento=request.form['txtLugarNacimiento']
+    _email=request.form['txtEmail']
+    _foto=request.files['txtFoto']
+
+
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+    sql = '''INSERT INTO registroDatos (nombre, apellidoPaterno, apellidoMaterno, edad, lugarNacimiento, email, foto) 
+             VALUES (%s, %s, %s, %s, %s, %s, %s);'''
+    datos = (_nombre, _apellidoPaterno, _apellidoMaterno, _edad, _lugarNacimiento, _email, _foto.filename)
+    cursor.execute(sql,datos)
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return render_template('registroEmpleados/index.html')
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
